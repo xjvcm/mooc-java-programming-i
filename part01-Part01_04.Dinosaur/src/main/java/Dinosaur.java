ddd7ea1b1bf77a8c1661ae9d795b11d1ae564246
Yanni App13 <jonvincruman@gmail.com>
2020-03-22 15:44:14 -0700

    added one sentence story to Dinosaur

--- part01-Part01_04.Dinosaur/src/main/java/Dinosaur.java
+++ part01-Part01_04.Dinosaur/src/main/java/Dinosaur.java
@@ -2,6 +2,9 @@
 public class Dinosaur {
 
     public static void main(String[] args) {
-        // Write your program here
+        // Write one sentence story about a dinosaur
+        System.out.println("Once upon a time");
+        System.out.println("there was");
+        System.out.println("a dinosaur");
     }
 }
