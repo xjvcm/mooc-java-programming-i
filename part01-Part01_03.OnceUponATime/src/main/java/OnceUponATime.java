6cc0677b533c70cddb9a9016c0899967eafa0cff
Yanni App13 <jonvincruman@gmail.com>
2020-03-22 15:36:28 -0700

    added one sentence story to Once Upon a Time

--- part01-Part01_03.OnceUponATime/src/main/java/OnceUponATime.java
+++ part01-Part01_03.OnceUponATime/src/main/java/OnceUponATime.java
@@ -2,6 +2,9 @@
 public class OnceUponATime {
 
     public static void main(String[] args) {
-        // Write your program here
+        // Print a one sentence story on three lines
+        System.out.println("Once upon a time");
+        System.out.println("there was");
+        System.out.println("a program");
     }
 }
