4fbcf0cce6bcd97ce718fe6e99afc8d8e3540655
Yanni App13 <jonvincruman@gmail.com>
2020-03-22 14:57:15 -0700

    updated leap year

--- part01-Part01_36.LeapYear/src/main/java/LeapYear.java
+++ part01-Part01_36.LeapYear/src/main/java/LeapYear.java
@@ -6,19 +6,15 @@ public class LeapYear {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         
-        int year;
         boolean leap = false;
         
-        System.out.print("Type a year: ");
-        year = Integer.valueOf(scan.nextLine());
+        System.out.print("Give a year: ");
+        int year = Integer.valueOf(scan.nextLine());
         
-        boolean leapDiv4 = year % 4 == 0 && year % 100 != 0;
-        boolean leapDiv400 = year % 400 == 0;
-        
-        if (leapDiv4 || leapDiv400) {
-            System.out.println("The year is a leap year.");
-        } else {
-            System.out.println("The year is not a leap year.");
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            leap = true;
         }
+        
+        System.out.println("The year is " + (leap ? "" : "not ") + "a leap year.");
     }
 }
