1007a594936d73a9bacdcd19bce78a8c6f065d56
Yanni App13 <jonvincruman@gmail.com>
2020-03-23 05:46:18 +0100

    added content to Greeting

--- part01-Part01_08.Greeting/src/main/java/Greeting.java
+++ part01-Part01_08.Greeting/src/main/java/Greeting.java
@@ -6,7 +6,12 @@ public class Greeting {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Write your program here
+        // Prompt user to enter their name
+        System.out.println("What's your name?");
+        String name = scanner.nextLine();
+        
+        // Print greeting with name
+        System.out.println("Hi " + name);
 
     }
 }
