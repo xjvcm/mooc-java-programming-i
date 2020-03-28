3fca36f5ada696d441d7c523e4afefcdf6bca32f
Yanni App13 <jonvincruman@gmail.com>
2020-03-28 04:34:54 -0700

    added content to Square Root of Sum

--- part02-Part02_02.SquareRootOfSum/src/main/java/SquareRootOfSum.java
+++ part02-Part02_02.SquareRootOfSum/src/main/java/SquareRootOfSum.java
@@ -5,6 +5,14 @@ public class SquareRootOfSum {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        
+        int int1 = Integer.valueOf(scanner.nextLine());
+        int int2 = Integer.valueOf(scanner.nextLine());
+        
+        int sum = int1 + int2;
+        
+        double squareRoot = Math.sqrt(sum);
+        
+        System.out.println(squareRoot);
     }
 }
