5f53508f8133cf60a75f68d68702c393b09f7b86
Yanni App13 <jonvincruman@gmail.com>
2020-03-28 04:41:33 -0700

    added content to Absolute value

--- part02-Part02_03.AbsoluteValue/src/main/java/AbsoluteValue.java
+++ part02-Part02_03.AbsoluteValue/src/main/java/AbsoluteValue.java
@@ -5,6 +5,14 @@ public class AbsoluteValue {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
 
+        int number = Integer.valueOf(scanner.nextLine());
+
+        if (number < 0) {
+            number = number * -1;
+        }
+        
+        System.out.println(number);
     }
 }
