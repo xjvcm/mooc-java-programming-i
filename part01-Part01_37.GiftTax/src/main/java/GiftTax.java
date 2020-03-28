329fc1c3962bd12cb6535aa2e71ec6d9215a36e8
Yanni App13 <jonvincruman@gmail.com>
2020-03-28 02:49:29 -0700

    added content to gift tax

--- part01-Part01_37.GiftTax/src/main/java/GiftTax.java
+++ part01-Part01_37.GiftTax/src/main/java/GiftTax.java
@@ -12,12 +12,12 @@ public class GiftTax {
         if (value < 5000) {
             System.out.println("No tax!");
         } else {
-            System.out.printf("Tax: %.1f", calcTax(value));
+            System.out.print("Tax: " + calcTax(value));
         }
     }
     
     public static double calcTax(double value) {
-        double tax = 0.0;
+        double tax = 0.00;
         
         if (value >= 1000000) {
             tax = (142100 + (value - 1000000) * .17);
@@ -25,8 +25,8 @@ public class GiftTax {
             tax = (22100 + (value - 200000) * .15);
         } else if (value >= 55000 && value < 200000) {
             tax = (4700 + (value - 55000) * .12);
-        } else if (value >= 25000 && value < 5500) {
-            tax = (1700 + (value - 25000) * .1);
+        } else if (value >= 25000 && value < 55000) {
+            tax = (1700 + (value - 25000) * .10);
         } else {
             tax = (100 + (value - 5000) * .08);
         }
