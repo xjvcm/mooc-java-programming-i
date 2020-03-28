d4e54ebc5764cc123b3fbd25ef5cb22b9a5f4957
Yanni App13 <jonvincruman@gmail.com>
2020-03-28 05:41:29 -0700

    added contnt to carry on

--- part02-Part02_05.CarryOn/src/main/java/CarryOn.java
+++ part02-Part02_05.CarryOn/src/main/java/CarryOn.java
@@ -5,6 +5,14 @@ public class CarryOn {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        
+        // loop until user says no
+        while(true) {
+            System.out.println("Carry on?");
+            String input = scanner.nextLine();
+            if (input.equals("no")) {
+                break;
+            }
+        }
     }
 }
